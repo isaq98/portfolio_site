4dabdd75a176d7c18c6df8bca64cea626a90715a
Sameer Isaq <shisaq@dons.usfca.edu>
2023-01-12 16:54:32 -0800

    Adding section for work experience

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Overview from './Components/Overview';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
+import Experience from './Components/Experience';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Navbar />
     <Overview />
     <About />
+    <Experience />
    </main>
   );
 }
---
+++ src/Components/Experience/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './_Experience.scss';
+
+function Experience() {
+    return(
+        <section className="experience">
+            <div className="experience-container">
+                <h2>Experience</h2>
+            </div>
+        </section>
+    )
+}
+
+export default Experience;
