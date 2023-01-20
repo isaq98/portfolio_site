80a33d47840c97a5926d2a2d1366704187f1bfdf
Sameer Isaq <shisaq@dons.usfca.edu>
2023-01-20 13:00:55 -0800

    Cleaning up projects section

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -10,6 +10,9 @@ function Projects() {
                 <a href={project.link}>
                 <div key={project.title} className={`project ${index}`}>
                 <header>
+                    <div className="folder-svg">
+                        <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="-10 -10 170 40" fill="none" stroke="#F2AA4CFF" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+                    </div>
                     <h3 className="project-title">{project.title}</h3>
                     <div className="project-description-container">
                         <p>{project.description}</p>
